c684b008fe2344bffcc4ec9c907b1fafb3d06ef5
Akhand <148524224+Akhandbhadouria@users.noreply.github.com>
2025-12-02 16:06:18 +0530

    Update settings.py

--- tax_fare_django/settings.py
+++ tax_fare_django/settings.py
@@ -171,13 +171,6 @@ CHANNEL_LAYERS = {
     }
 }
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'akhandbhadouria1707@gmail.com'
-# EMAIL_HOST_PASSWORD = 'ebkvmnmqthfomlho'
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 ACCOUNT_EMAIL_VERIFICATION = "none"
